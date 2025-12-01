6dd8ebd0f9ecf4c838dbd2864dafbdc06219718e
Andrew Acevedo Mirena <acvdoandrew@gmail.com>
2025-12-01 16:23:56 -0500

    initial commit for aoc 2025 in rust

---
+++ README.md
@@ -0,0 +1,7 @@
+# Advent of Code 2025 - Rust
+
+My solutions for Advent of Code 2025 in Rust.
+
+- One crate, multiple binaries in `src/bin`
+- Inputs are local-only and ignore via `.gitignore`
+- Each day: `cargo run --bin dayXX`
---
+++ src/bin/day01.rs
@@ -0,0 +1,15 @@
+use std::fs;
+
+fn main() {
+    let input = fs::read_to_string("inputs/day01.txt").expect("failed to read input");
+    println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
+
+    fn part1(input: &str) -> i64 {
+        todo!();
+    }
+
+    fn part2(input: &str) -> i64 {
+        todo!();
+    }
+}
